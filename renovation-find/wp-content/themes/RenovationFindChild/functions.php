83c92ac9a5b5e3df5239a58ca26faac1f29454f2
Tyler Findlay <tyler@tylerfindlay.com>
2017-10-14 13:49:35 +0200

    Updated with final changes

--- renovation-find/wp-content/themes/RenovationFindChild/functions.php
+++ renovation-find/wp-content/themes/RenovationFindChild/functions.php
@@ -3,7 +3,7 @@ function theme_enqueue_styles() {
 
     $parent_style = 'parent-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', array('asher'), '1.1' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
