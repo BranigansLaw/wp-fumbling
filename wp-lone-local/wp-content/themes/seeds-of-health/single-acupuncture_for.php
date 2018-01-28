15fdd9a6adfc52af77c0b6324ce69a61b54a5fd5
Tyler Findlay <tyler@tylerfindlay.com>
2018-01-28 16:05:55 -0700

    Added the new single pages for post types
    Added the new single pages for post types
    Fixed an issue with sidebar CSS
    Added css to make the HTML same as the footer sol it doesn't look weird
    on large pages
    Fixed "page" so that it uses the section stuff

--- wp-lone-local/wp-content/themes/seeds-of-health/single-acupuncture_for.php
+++ wp-lone-local/wp-content/themes/seeds-of-health/single-acupuncture_for.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying a acupuncture_for post type
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -22,9 +17,20 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<?php
 			while ( have_posts() ) : the_post();
-				
-				get_template_part( 'template-parts/header/page', 'header' );
 
+				//get_template_part( 'template-parts/header/page', 'header' );
+
+			?>
+
+				<section>
+					<div class="inner">
+			<?php
+				get_template_part( 'template-parts/faq/faq-single', 'display' );
+			?>
+					</div>
+				</section>
+
+			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
