a9bde2e08f1732cfa16d25f1d8ed648780655d14
Tyler Findlay <tyler@tylerfindlay.com>
2018-01-29 18:47:21 -0700

    Added the header image to the header

--- wp-lone-local/wp-content/themes/seeds-of-health/template-parts/header/page-header.php
+++ wp-lone-local/wp-content/themes/seeds-of-health/template-parts/header/page-header.php
@@ -8,21 +8,14 @@
  * @version 1.0
  */
 ?>
-<header class="seeds-subtitle-header">
+<header class="seeds-subtitle-header" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(),'full') ?>')">
 	<div class="inner">
 		<h1><?php the_title() ?></h1>
-		<div class="header-image-container">
-			<?php
-				if ( has_post_thumbnail() ) {
-					echo wp_get_attachment_image( get_post_thumbnail_id(), 'medium' );
-				}
-				else { ?>
-					<div class="empty-box">Empty Box</div>
-			<?php
-				}
-			?>
-		</div>
+	</div>
+</header>
+<section class="seeds-subtitle-header-content">
+	<div class="inner">
 		<h2><?php the_field( 'subtitle' ) ?></h2>
 		<div><?php the_content() ?></div>
 	</div>
-</header>
+</section>
